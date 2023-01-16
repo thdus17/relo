a61ec07caf3c368fc1db2ce7b863ba6cbf9141ab
keyhyeong <jgh112415@gmail.com>
2023-01-16 10:09:44 +0900

    test

---
+++ relo/src/main/java/com/relo/sizes/SizesVo.java
@@ -0,0 +1,17 @@
+package com.relo.sizes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class SizesVo {
+	private int size_category_num;
+	private String size_category_name;
+}
