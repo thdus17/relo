60344f3abc63ddf0385a436db84de0ed10628edc
KyunglaeKim <klkim3272@gmail.com>
2023-01-16 17:30:39 +0900

    Zzim Service 제외 완성 후 업로드

--- relo/src/main/java/com/relo/zzim/ZzimDAO.java
+++ relo/src/main/java/com/relo/zzim/ZzimDAO.java
@@ -1,9 +1,6 @@
 package com.relo.zzim;
 
-import java.util.ArrayList;
-
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import java.util.List;
 
 import com.relo.exception.FindException;
 
@@ -13,39 +10,27 @@ public interface ZzimDAO {
 	 * 사용자의 상품 찜목록을 검색한다
 	 * 
 	 * @param id : 사용자 ID
-	 * @return : 사용자의 상품 찜목록
+	 * @return : List<ZzimDTO> 사용자의 상품 찜목록
 	 * @throws FindException
 	 */
-	@Select("select z.id, p.p_num, s_color, s_name, s_type, s_color, s_hope_price, s_grade, p_end_date \r\n"
-			+ "from product p, stock s, zzim z  \r\n" + "where p.p_num = z.p_num and s.s_num = p.s_num and z.id=#{id}")
-	ArrayList<ZzimVo> selectById(@Param("id") String id) throws FindException;
-
-	/**
-	 * 상품의 찜등록 갯수를 구한다(순위 계산용)
-	 * 
-	 * @param p_num : 상품번호
-	 * @return 상품번호에 해당하는 상품의 찜목록 개수
-	 * @throws FindException
-	 */
-	public ArrayList<ZzimVo> CountByProduct(int p_num) throws FindException;
+	public List<ZzimDTO> selectById(String id) throws FindException;
 
 	/**
 	 * 사용자가 상품을 찜목록에 추가한다
 	 * 
-	 * @param id    : 사용자 ID
-	 * @param p_num : 상품 번호
+	 * @param ZzimVo(Id,P_num)
 	 * @throws FindException
 	 */
-	public void InsertZzim(String id, int p_num) throws FindException;
+	public void insertZzim(ZzimVo vo) throws FindException;
 
 	/**
 	 * 사용자가 상품을 찜목록에서 삭제한다
 	 * 
-	 * @param id    : 사용자 ID
-	 * @param p_num : 상품 번호
+	 * @param id               : 사용자 ID
+	 * @param ZzimVo(Id,P_num)
 	 * @throws FindException
 	 */
-	public void DeleteZzim(String id, int p_num) throws FindException;
+	public void deleteZzim(ZzimVo vo) throws FindException;
 
 	/**
 	 * 상품 목록에서 삭제되었을 때 모든 사용자의 찜목록에서 삭제한다
@@ -53,5 +38,6 @@ public interface ZzimDAO {
 	 * @param p_num : 상품번호
 	 * @throws FindException
 	 */
-	public void DeleteZzimAll(int p_num) throws FindException;
+	public void deleteZzimAll(int pNum) throws FindException;
+
 }
