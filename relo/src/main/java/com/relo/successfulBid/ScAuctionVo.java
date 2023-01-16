807147a52659b82cdacf09ba4fb5b35ca3ba7cb1
thdus17 <dusghk1717@gmail.com>
2023-01-16 09:58:53 +0900

    commit Vo

---
+++ relo/src/main/java/com/relo/successfulBid/ScAuctionVo.java
@@ -0,0 +1,20 @@
+package com.relo.successfulBid;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class ScAuctionVo {
+	private int a_num;
+	private Date c_time;
+}
