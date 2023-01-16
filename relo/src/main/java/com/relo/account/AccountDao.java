7c7ce828964b295e09e35a1805e66706de35261b
keyhyeong <121778566+keyhyeong@users.noreply.github.com>
2023-01-16 20:44:30 +0900

    Delete relo directory
    삭제

--- relo/src/main/java/com/relo/account/AccountDao.java
+++
@@ -1,9 +0,0 @@
-package com.relo.account;
-
-
-
-public interface AccountDao {
-	//1-2 판매자 계좌등록
-	public void insertAccount(AccountVo vo);
-	
-}
--- relo/src/main/java/com/relo/auction/AuctionDao.java
+++
@@ -1,9 +0,0 @@
-package com.relo.auction;
-
-import java.util.List;
-
-import com.relo.product.ProductVo;
-
-public interface AuctionDao {
-	public List<ProductVo> selectByPStatus();
-}
