24ee0a3788c28b1e50a5f02eb24790d471bb7f15
Kwkwl <yel0129k@gmail.com>
2023-01-19 14:48:06 +0900

    id 중복 확인, id or pwd 찾기, login, logout Handler 추가

---
+++ src/main/java/com/relo/handler/member/MemberLogin.java
@@ -0,0 +1,58 @@
+package com.relo.handler.member;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.relo.exception.FindException;
+import com.relo.handler.Handler;
+import com.relo.member.MemberService;
+import com.relo.member.MemberVo;
+
+public class MemberLogin implements Handler {
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("application/json;charset=utf-8");
+		response.addHeader("Access-Control-Allow-Origin", "*");
+
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+
+		ObjectMapper mapper = new ObjectMapper();
+		MemberService service = new MemberService();
+		try {
+			MemberVo m = service.getOne(id);
+			String message = "";
+			if (m == null) {
+				message = "가입된 회원이 아님";
+			} else {
+				if (!id.equals(m.getId()) || !pwd.equals(m.getPwd()))
+					message = "아이디, 비밀번호 불일치";
+				else {
+					HttpSession session = request.getSession();
+					session.setAttribute("loginId", id);
+					if (m.getType() == 1) // 관리자 모드
+						return null;
+					else
+						return null; // 판매자 및 구매자 모드
+				}
+			}
+			String jsonStr = mapper.writeValueAsString(message);
+			return jsonStr;
+		} catch (FindException e) {
+			e.printStackTrace();
+			Map<String, String> map = new HashMap<>();
+			map.put("msg", e.getMessage());
+			String jsonStr = mapper.writeValueAsString(map);
+			return jsonStr;
+		}
+	}
+}
